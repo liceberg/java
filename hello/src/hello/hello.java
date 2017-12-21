6ac84ffccbb4b2c4cc757862ae7cb2de4ec3aa98
liyb <lyb510@139.com>
2017-12-21 20:45:18 +0800

    hello
    v1.0

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.util.Date;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.currentTimeMillis());
+		System.out.println(new Date());
+
+	}
+
+}
