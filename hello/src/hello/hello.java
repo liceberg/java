1dcdb506ec4f1b3b82a2d110bb21657cbe3b4fe2
liyb <lyb510@139.com>
2017-12-21 21:01:07 +0800

    Revert "Update hello.java"
    This reverts commit 6b85906470a36e1bf65412d7347409ed6ac863d9.

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -6,7 +6,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(System.currentTimeMillis()+"time");
+		System.out.println(System.currentTimeMillis());
 		System.out.println(new Date());
 
 	}
